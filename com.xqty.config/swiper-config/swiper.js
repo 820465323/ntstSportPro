20e09774113b4c94075c27d5e8e850ff6f6aea12
EDZ <17319132391@163.com>
2018-09-29 15:44:39 +0800

    swiper第二天

---
+++ com.xqty.config/swiper-config/swiper.js
@@ -0,0 +1,28 @@
+var mySwiper = new Swiper ('.swiper-container', {
+    //切换方式
+    effect : 'fade',
+    //水平
+    direction : 'vertical',
+    hashNavigation: true,
+    //鼠标切换
+    mousewheel: true,
+    //点击切换速度
+    speed:1000,
+    //自动切换速度
+    autoplay : {
+        delay:3000
+    },
+    loop: true,
+    // 如果需要分页器
+    pagination: {
+     el: '.swiper-pagination',
+     //分页器按钮
+     dynamicBullets :true,
+        clickable:true
+     },
+    // 如果需要前进后退按钮
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
