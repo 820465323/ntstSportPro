38d62d6c1ed6bf2d0f7de70ebf1f177b9b21ea45
EDZ <17319132391@163.com>
2018-09-29 16:36:15 +0800

    swiper第二天

--- com.xqty.config/swiper-config/swiper.js
+++ com.xqty.config/swiper-config/swiper.js
@@ -15,11 +15,11 @@ var mySwiper = new Swiper ('.swiper-container', {
     loop: true,
     // 如果需要分页器
     pagination: {
-     el: '.swiper-pagination',
-     //分页器按钮
-     dynamicBullets :true,
+        el: '.swiper-pagination',
+        //分页器按钮
+        dynamicBullets :true,
         clickable:true
-     },
+    },
     // 如果需要前进后退按钮
     navigation: {
         nextEl: '.swiper-button-next',
