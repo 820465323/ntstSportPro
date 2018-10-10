e0f321823a6cfe118588dbca1cfd706ac1555307
zzw <17319132391@163.com>
2018-10-10 18:06:23 +0800

    2018 10.10 开始页面填充并自定义滚动轴

--- com.xqty.config/swiper-config/swiper.js
+++ com.xqty.config/swiper-config/swiper.js
@@ -1,63 +1,63 @@
-var mySwiper = new Swiper ('.swiper-container', {
+var mySwiper = new Swiper('.swiper-container', {
     //监听swiper偏移量
-    on:{
-        setTranslate: function(translate){
+    on: {
+        setTranslate: function (translate) {
             var nav1 = document.querySelector('#nav-a1');
             var nav2 = document.querySelector('#nav-a2');
             var pro1 = document.querySelector('#swiper-slide');
             var pro2 = document.querySelector('#swiper-slide2');
-            if(translate=='-657'){
-                nav2.style='color:yellow';
-                pro1.style='transform: translateY(-100%);transition:all 1.5s;display:block';
-                pro2.style='transform: translateX(100%);transition:all 1.9s;';
-                nav1.style='color:block';
-            }else if(translate=='0'){
-                nav2.style='color:block';
-                pro1.style='position: absolute;margin-top: 60%;border: 1px solid brown;width: 300px;height: 400px;';
-                pro2.style='position: absolute;top: 20%;left: -30%;border: 1px solid brown;width: 300px;height: 400px;';
-                nav1.style='color:yellow';
+            if (translate == '-765') {
+                nav2.style = 'color:yellow';
+                pro1.style = 'transform: translateY(-100%);transition:all 1.5s;display:block';
+                pro2.style = 'transform: translateX(100%);transition:all 1.9s;';
+                nav1.style = 'color:block';
+            } else if (translate == '0') {
+                nav2.style = 'color:block';
+                pro1.style = 'position: absolute;margin-top: 60%;border: 1px solid brown;width: 300px;height: 400px;';
+                pro2.style = 'position: absolute;top: 20%;left: -30%;border: 1px solid brown;width: 300px;height: 400px;';
+                nav1.style = 'color:yellow';
             }
             setTimeout(function () {
                 //获取当前模块的索引进行判断
-                if(mySwiper.activeIndex==0){
+                if (mySwiper.activeIndex == 0) {
                     mySwiper.mousewheel.disable();
-                }else if(mySwiper.activeIndex!=0){
+                } else if (mySwiper.activeIndex != 0) {
                     console.log('!=0!=0');
                     mySwiper.mousewheel.enable();
                 }
-            },1);
+            }, 1);
         }
     },
     //切换方式
-    effect : 'fade',
+    effect: 'fade',
     fadeEffect: {
         crossFade: true,
     },
     //轮播顺序
-    direction : 'vertical',
+    direction: 'vertical',
     hashNavigation: {
         watchState: true
     },
-    loop:false,
+    loop: false,
     //点击切换速度
-    speed:1000,
+    speed: 1000,
     //鼠标滑轮控制
     mousewheel: true,
     //自动切换速度
-   /* autoplay : {
-        delay:3000
-    },*/
+    /* autoplay : {
+     delay:3000
+     },*/
     // 如果需要分页器
     pagination: {
         el: '.swiper-pagination',
         //分页器按钮
-        dynamicBullets :true,
-        clickable:true,
-        hiddenClass : 'oneone'
+        dynamicBullets: true,
+        clickable: true,
+        hiddenClass: 'oneone'
     }
     // 如果需要前进后退按钮
-   /* navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },*/
+    /* navigation: {
+     nextEl: '.swiper-button-next',
+     prevEl: '.swiper-button-prev',
+     },*/
 });
